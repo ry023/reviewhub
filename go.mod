995bc4feed97234513d9b42944441374ccc1e85f
ry023 <ry023.blue@gmail.com>
2024-03-04 18:27:53 +0900

    Add slack notifier

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/ry023/reviewhub
 
 go 1.21.6
+
+require github.com/slack-go/slack v0.12.5
+
+require github.com/gorilla/websocket v1.4.2 // indirect
---
+++ notifiers/slack/notifier.go
@@ -0,0 +1,112 @@
+package slack
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ry023/reviewhub/reviewhub"
+	"github.com/slack-go/slack"
+)
+
+const MetaDataSlackId = "slack_id"
+
+type SlackNotifier struct {
+	// Bot Token or User Token
+	ApiToken string
+	// Destination
+	Channel string
+}
+
+func NewSlackNotifier(token string, channel string) *SlackNotifier {
+	return &SlackNotifier{
+		ApiToken: token,
+		Channel:  channel,
+	}
+}
+
+func (n *SlackNotifier) NotifyReviewPages(user reviewhub.User, ls []reviewhub.ReviewList) error {
+	cli := slack.New(n.ApiToken)
+
+	slackId, ok := user.MetaData[MetaDataSlackId]
+	if !ok {
+		// This user not slack information
+		return nil
+	}
+
+	b := []slack.Block{
+		// Header Block
+		slack.NewHeaderBlock(
+			slack.NewTextBlockObject(
+				slack.PlainTextType,
+				fmt.Sprintf("Notification For You (%s)", user.Name),
+				false,
+				false,
+			),
+		),
+	}
+
+	for _, c := range ls {
+		// Page List
+		b = append(b, buildPageListBlock(c))
+		// Divider
+		b = append(b, slack.NewDividerBlock())
+	}
+
+	_, err := cli.PostEphemeral(n.Channel, slackId, slack.MsgOptionBlocks(b...))
+	if err != nil {
+		log.Printf("Failed to send to %s: %v", user.Name, err)
+	}
+
+	return nil
+}
+
+func buildPageListBlock(r reviewhub.ReviewList) slack.Block {
+	// List Name
+	name := slack.NewRichTextSection(
+		slack.NewRichTextSectionTextElement(
+			r.Name,
+			&slack.RichTextSectionTextStyle{
+				Bold: true,
+			},
+		),
+	)
+
+	// Early return if no review page!
+	if len(r.Pages) == 0 {
+		s := slack.NewRichTextSection(
+			// emoji
+			slack.NewRichTextSectionEmojiElement("tada", 2, nil),
+			// message
+			slack.NewRichTextSectionTextElement(
+				"There are no pages you need to review! Thank you for your cooperation!",
+				&slack.RichTextSectionTextStyle{Italic: true},
+			),
+		)
+		return slack.NewRichTextBlock("", name, s)
+	}
+
+	// Page List
+	var els []slack.RichTextElement
+	for _, page := range r.Pages {
+		// Building List Element...
+		s := slack.NewRichTextSection(
+			// Page URL
+			slack.NewRichTextSectionLinkElement(
+				page.Url,
+				page.Title,
+				&slack.RichTextSectionTextStyle{
+					Bold: true,
+				},
+			),
+			// Owner
+			slack.NewRichTextSectionTextElement(
+				fmt.Sprintf("(by %s)", page.Owner.Name),
+				&slack.RichTextSectionTextStyle{},
+			),
+		)
+		els = append(els, s)
+	}
+	list := slack.NewRichTextList(slack.RTEListBullet, 0, els...) // Sum up to RichTextList block
+
+	return slack.NewRichTextBlock("", name, list)
+}
