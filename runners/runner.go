f62bd7a5dcc97ccde287e50917714a198b4ed070
ry023 <ry023.blue@gmail.com>
2024-03-05 11:35:54 +0900

    Add reviewhub

--- runners/runner.go
+++ runners/runner.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 
 	"github.com/ry023/reviewhub/notifiers/slack"
+	"github.com/ry023/reviewhub/retrievers/notion"
 	"github.com/ry023/reviewhub/reviewhub"
 )
 
 type ReviewHubRunner struct {
 	config reviewhub.Config
-	users  []reviewhub.User
+
+	users      []reviewhub.User
+	notifiers  []reviewhub.Notifier
+	retrievers []reviewhub.Retriever
 }
 
 var ErrNotBuiltIn error = fmt.Errorf("This type is not built-in")
@@ -22,6 +26,19 @@ func (r *ReviewHubRunner) parseBuiltinNotifier(config *reviewhub.NotifierConfig)
 	switch config.Type {
 	case "slack":
 		return slack.New(config)
+	}
+
+	return nil, ErrNotBuiltIn
+}
+
+func (r *ReviewHubRunner) parseBuiltinRetriever(config *reviewhub.RetrieverConfig) (reviewhub.Retriever, error) {
+	if config.Type == "" {
+		return nil, fmt.Errorf("'type' field empty")
+	}
+
+	switch config.Type {
+	case "notion":
+		return notion.New(config)
 	}
 
 	return nil, ErrNotBuiltIn
