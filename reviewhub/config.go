ff6f7011b3f93ef8e5d9b20a99820db104648867
ry023 <ry023.blue@gmail.com>
2024-03-05 11:30:47 +0900

    ParseMetaData

--- reviewhub/config.go
+++ reviewhub/config.go
@@ -1,5 +1,10 @@
 package reviewhub
 
+import (
+	"github.com/go-playground/validator"
+	"github.com/go-yaml/yaml"
+)
+
 type Config struct {
 	Retrievers []RetrieverConfig
 	Notifiers  []NotifierConfig
@@ -9,11 +14,30 @@ type Config struct {
 type NotifierConfig struct {
 	Name     string
 	Type     string
-	MetaData map[string]string
+	MetaData any
 }
 
 type RetrieverConfig struct {
 	Name     string
 	Type     string
-	MetaData map[string]string
+	MetaData any
+}
+
+func ParseMetaData[T any](raw any) (*T, error) {
+	b, err := yaml.Marshal(raw)
+	if err != nil {
+		return nil, err
+	}
+
+	var m T
+	err = yaml.Unmarshal(b, &m)
+	if err != nil {
+		return nil, err
+	}
+
+	err = validator.New().Struct(m)
+	if err != nil {
+		return nil, err
+	}
+	return &m, nil
 }
