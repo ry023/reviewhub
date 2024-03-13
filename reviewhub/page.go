6b314c715d5871c51a5d6cc9c9e0a9d0d4728c8c
ry023 <ry023.blue@gmail.com>
2024-03-13 13:02:40 +0900

    go fmt

--- reviewhub/page.go
+++ reviewhub/page.go
@@ -31,11 +31,11 @@ type ReviewList struct {
 }
 
 func FilterByReviewer(ls []ReviewList, u User, includeApproved bool) []ReviewList {
-  var filtered []ReviewList
-  for _, l := range ls {
-    filtered = append(filtered, l.FilterByReviewer(u, includeApproved))
-  }
-  return filtered
+	var filtered []ReviewList
+	for _, l := range ls {
+		filtered = append(filtered, l.FilterByReviewer(u, includeApproved))
+	}
+	return filtered
 }
 
 func (r ReviewList) FilterByReviewer(u User, includeApproved bool) ReviewList {
